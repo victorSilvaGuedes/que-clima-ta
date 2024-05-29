5dadcdaf8e6807c77792fb1684c91f06a5deb353
Victor Silva Guedes <sguedes.victor@gmail.com>
2024-05-29 16:43:52 -0300

    refactor: text positions

--- src/components/sunset-sunrise/index.tsx
+++ src/components/sunset-sunrise/index.tsx
@@ -7,21 +7,25 @@ import { Box } from '../box'
 export function SunsetSunrise() {
   const { forecast } = useGlobalContext()
   return (
-    <Box className="flex flex-col flex-1 gap-4 items-center justify-evenly">
-      <div className="flex flex-col items-center gap-2">
+    <Box className="flex flex-col flex-1 gap-4 justify-evenly">
+      <div className="flex flex-col gap-2">
         <div className="flex items-center gap-2">
           <Sunrise />
           <p className="text-xl font-semibold">Nascer do sol</p>
         </div>
-        <p className="text-5xl font-semibold">{forecast.sunriseFormatted}</p>
+        <p className="text-5xl font-semibold text-center">
+          {forecast.sunriseFormatted}
+        </p>
       </div>
 
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col gap-2">
         <div className="flex items-center gap-2">
           <Sunset />
           <p className="text-xl font-semibold">Pôr do sol</p>
         </div>
-        <p className="text-5xl font-semibold">{forecast.sunsetFormatted}</p>
+        <p className="text-5xl font-semibold text-center">
+          {forecast.sunsetFormatted}
+        </p>
       </div>
     </Box>
   )
